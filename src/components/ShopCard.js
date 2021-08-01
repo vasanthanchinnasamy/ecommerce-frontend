c6bcb3d54ff05cf178c5bbbf1a84d0b23e841c04
unknown <sudheeshv2010@gmail.com>
2021-08-01 17:19:55 +0530

    refs initial commit store lst

---
+++ src/components/ShopCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '200px',
+    minWidth : '200px',
+    minHeight : '240px',
+    maxHeight : '240px',
+    margin : "8px 8px 8px 8px"
+  },
+  media: {
+    height: 140,
+  },
+
+
+});
+
+export default function ShopCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card id={props.id} onClick = {props.clickEvent} className={classes.root}>
+      <CardActionArea key={props.id}>
+        <CardMedia id={props.id}
+          className={classes.media}
+          image={props.imageUrl}
+          title={props.title}
+        />
+        <CardContent id={props.id}>
+          <Typography id={props.id} gutterBottom variant="h5" component="h2">
+          {props.title}
+          </Typography>
+          <Typography id={props.id}  variant="body2" color="textSecondary" component="p">
+          {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/pages/StoreListPage.js
+++ src/pages/StoreListPage.js
@@ -1,9 +1,52 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
+import ShopAppBar from "../components/ShopAppBar.js";
+import ShopCard from "../components/ShopCard.js";
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+  divClass: {
+    marginTop: '72px',
+  }
+});
 export const StoreListPage = () => {
+  const [data, setData] = useState(null); 
+  const classes = useStyles();
+
+  useEffect(()=>{
+    fetch(`https://ecommerce-backend-java.herokuapp.com/shop/getAll`)
+    .then((response) => response.json())
+    .then(setData);
+  },[]);
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+
+  function getUrl(i){
+    return "https://source.unsplash.com/random/200x200?sig="+i;
+  }
+
+if(!data){
+  return <div></div>;
+}
   return (
     <div>
-      <h3>StoreListPage </h3>
+      <ShopAppBar title = "ECart"></ShopAppBar>
+      <div className = {classes.divClass}>
+        <Grid container
+  direction="row"
+  justifyContent="flex-start"
+  alignItems="center">
+      {data.map((product,i)=><div 
+      onClick={(e)=> {
+        openInNewTab("/products/"+e.target.getAttribute("id"));
+        }} 
+        key={product.shopId} ><ShopCard imageUrl={getUrl({i})} title = {product.shopName} id={product.shopId} description = {product.shopDescription}></ShopCard></div>)}
+      </Grid>
+      </div>
     </div>
   );
 };
